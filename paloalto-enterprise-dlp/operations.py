add5a7f7d67ca26dfb7a2fe1f319049a6075dada
cs-shubham-morankar <smorankar@fortinet.com>
2024-02-09 10:50:05 +0530

    First Commit

--- paloalto-enterprise-dlp/operations.py
+++ paloalto-enterprise-dlp/operations.py
@@ -1,3 +1,10 @@
+"""
+Copyright start
+MIT License
+Copyright (c) 2024 Fortinet Inc
+Copyright end
+"""
+
 from .paloalto_api_auth import *
 from connectors.core.connector import get_logger, ConnectorError
 import requests, json
